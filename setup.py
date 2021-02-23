e8b3657c456719b5268c5907ce4556450255d247
timothy <timothy0626@gmail.com>
2021-02-23 08:42:52 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -11,19 +11,28 @@ FFLAG = ('-c', '-fPIC', '-O3')
 
 
 # compile all specified Fortran files to object codes
+print('===================')
+print('Generate object codes...')
 ofiles = []
 for f in ffiles:
     ofile = f.split('.')[0] + '.o'
     ofiles.append(ofile)
-    subprocess.run([FC, *FFLAG, f, '-o', ofile])
+    res = subprocess.run([FC, *FFLAG, f, '-o', ofile])
+    print(' '.join(res.args))
+print('===================')
 
 # link all object codes and put into a shared library
+print('Create a shared library...')
 soname = 'lib' + libname + '.so'
 res = subprocess.run([FC, *ofiles, '-shared', '-o', soname])
+print(' '.join(res.args))
+print('===================')
 
 
+# Cython wrapper
 files = ['hello_cython.pyx']
 
+# extension module object
 ext_module = [Extension(
         name="myhello",
         sources=files,
@@ -35,7 +44,16 @@ ext_module = [Extension(
         )
         ]
 
-
+# the setup command
+print('Create the extension module...')
 setup(name="myhello",
         ext_modules=cythonize(ext_module, language_level=3),
         )
+print('===================')
+
+
+# clean up manually
+print('Clean up...')
+cfiles = [f.split('.')[0] + '.c' for f in files]
+subprocess.run(['rm', '-r', 'build/', *ofiles, *cfiles])
+print('===================')
